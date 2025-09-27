3f9c409ad3c1e359d312164d25524e9ee543af40
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-09-27 17:29:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/ricardoramos/course/services/UserServices.java
@@ -0,0 +1,27 @@
+package com.ricardoramos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardoramos.course.entities.User;
+import com.ricardoramos.course.repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
