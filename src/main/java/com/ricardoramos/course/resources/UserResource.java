c41f6bcd3ed282eefaa4c57d702153db36f41ad1
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-09-27 16:53:14 -0300

    User entity and resource

---
+++ src/main/java/com/ricardoramos/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.ricardoramos.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardoramos.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "Maria@gmail.com", "999999999", "123456" );
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
