3273da8ae4be5441bb0abf121dd66183dd55790e
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-09-29 18:37:29 -0300

    Payment, one-to-one association

--- src/main/java/com/ricardoramos/course/config/TestConfig.java
+++ src/main/java/com/ricardoramos/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ricardoramos.course.entities.Category;
 import com.ricardoramos.course.entities.Order;
 import com.ricardoramos.course.entities.OrderItem;
+import com.ricardoramos.course.entities.Payment;
 import com.ricardoramos.course.entities.Product;
 import com.ricardoramos.course.entities.User;
 import com.ricardoramos.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
